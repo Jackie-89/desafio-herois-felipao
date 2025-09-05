d1471a4a1313b6a0ff840a8721496564875c033d
Jackie-89 <jaqueline.barcelo@hotmail.com>
2025-09-05 12:19:47 -0300

    tentativa2

---
+++ scr/index.js
@@ -0,0 +1,12 @@
+// Variáveis com os heróis e seus XP
+let timeHerois = ["Mulher-Maravilha", "Wolverine", "Homem-de-Ferro", "Homem-Aranha", "Tocha-Humana"];
+let xp = [
+  ["Mulher-Maravilha", 8000],
+  ["Wolverine", 9500],
+  ["Homem-de-Ferro", 10001],
+  ["Homem-Aranha", 4000],
+  ["Tocha-Humana", 3500]
+];
+
+console.log(timeHerois[0]);
+
