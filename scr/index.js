01427741a4ebac33dc77d37fd2ea1fa4efc5ed8e
Jackie-89 <jaqueline.barcelo@hotmail.com>
2025-09-05 16:53:21 -0300

    save final

--- scr/index.js
+++ scr/index.js
@@ -1,6 +1,6 @@
 // Variáveis com os heróis e seus XP
 let timeHerois = ["Mulher-Maravilha", "Wolverine", "Homem-de-Ferro", "Homem-Aranha", "Tocha-Humana"];
-let valorXp = [
+let xp = [
   ["Mulher-Maravilha", 8000],
   ["Wolverine", 9500],
   ["Homem-de-Ferro", 10001],
@@ -8,21 +8,23 @@ let valorXp = [
   ["Tocha-Humana", 3500]
 ];
 
-//concomitação
-console.log("Escolho " + timeHerois[0] + "com o xp de " + valorXp[0][1])
+// primeira concatenação 
+console.log("Escolho " + timeHerois[0] + " " + " com o XP de " + xp[0][1]);
 
-//proximas variaves
-if (valorXP <= 1000) return "Ferro";
-  else if (valorXP >= 1001 && valorXp <= 2000) return "Bronze";
-  else if (valorXP >= 2001 && valorXp <= 5000) return "Prata";
-  else if (valorXP >= 5001 && valorXp <= 7000) return "Ouro";
-  else if (valorXP >= 7001 && valorXp <= 8000) return "Diamante";
-  else if (valorXP >= 8001 && valorXp <= 9000) return "Ascendente";
-  else if (valorXP >= 9001 && valorXp <= 10000) return "Imortal";
-  else return "Radiante" (valorXp > 10000); 
-  
-//parte dois, tentando
-console.log("O Heróis escolhido é: " + timeHerois[0]);
-console.log("O nível XP do Heroí escolhido é: " + valorXp[0][1]);
-console.log(`${nomeHeroi} tem ${valorXp} de XP e está no nível ${nivelHeroi}`);
+//proximas variaves - Função para classificar o nível baseado no XP | parte 2
+function classificarNivel(xp) { 
+if (xp <= 1000) return "Ferro"; 
+else if (xp >= 1001 && xp <= 2000) return "Bronze"; 
+else if (xp >= 2001 && xp <= 5000) return "Prata"; 
+else if (xp >= 5001 && xp <= 7000) return "Ouro"; 
+else if (xp >= 7001 && xp <= 8000) return "Diamante"; 
+else if (xp >= 8001 && xp <= 9000) return "Ascendente"; 
+else if (xp >= 9001 && xp <= 10000) return "Imortal"; 
+else if (xp > 10000) return "Radiante"; 
+}
 
+// concatenação 2 | parte 3 - final?
+let nivelHeroi = classificarNivel(xp[0][1]);
+console.log("O Herói escolhido é: " + timeHerois[0]);
+console.log("O nível XP do Herói escolhido é: " + xp[0][1]);
+console.log(`${timeHerois[0]} + "tem " + ${xp[0][1]} + " de XP e está no nível " + ${nivelHeroi}`);  
