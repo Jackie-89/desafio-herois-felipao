ae843eac8abb0950675bece9bf3bfabf83d347fd
Jackie-89 <jaqueline.barcelo@hotmail.com>
2025-09-05 15:30:29 -0300

    tentativa2

--- scr/index.js
+++ scr/index.js
@@ -1,6 +1,6 @@
 // Variáveis com os heróis e seus XP
 let timeHerois = ["Mulher-Maravilha", "Wolverine", "Homem-de-Ferro", "Homem-Aranha", "Tocha-Humana"];
-let xp = [
+let valorXp = [
   ["Mulher-Maravilha", 8000],
   ["Wolverine", 9500],
   ["Homem-de-Ferro", 10001],
@@ -9,19 +9,20 @@ let xp = [
 ];
 
 //concomitação
-console.log("Escolho" + timeHerois[0] + "com o xp de " + xp[0][1])
+console.log("Escolho " + timeHerois[0] + "com o xp de " + valorXp[0][1])
 
 //proximas variaves
 if (valorXP <= 1000) return "Ferro";
-  else if (valorXP >= 1001 && valorXP <= 2000) return "Bronze";
-  else if (valorXP >= 2001 && valorXP <= 5000) return "Prata";
-  else if (valorXP >= 5001 && valorXP <= 7000) return "Ouro";
-  else if (valorXP >= 7001 && valorXP <= 8000) return "Diamante";
-  else if (valorXP >= 8001 && valorXP <= 9000) return "Ascendente";
-  else if (valorXP >= 9001 && valorXP <= 10000) return "Imortal";
-  else return "Radiante"; // Acima de 10000
-
+  else if (valorXP >= 1001 && valorXp <= 2000) return "Bronze";
+  else if (valorXP >= 2001 && valorXp <= 5000) return "Prata";
+  else if (valorXP >= 5001 && valorXp <= 7000) return "Ouro";
+  else if (valorXP >= 7001 && valorXp <= 8000) return "Diamante";
+  else if (valorXP >= 8001 && valorXp <= 9000) return "Ascendente";
+  else if (valorXP >= 9001 && valorXp <= 10000) return "Imortal";
+  else return "Radiante" (valorXp > 10000); 
   
-
-
+//parte dois, tentando
+console.log("O Heróis escolhido é: " + timeHerois[0]);
+console.log("O nível XP do Heroí escolhido é: " + valorXp[0][1]);
+console.log(`${nomeHeroi} tem ${valorXp} de XP e está no nível ${nivelHeroi}`);
 
